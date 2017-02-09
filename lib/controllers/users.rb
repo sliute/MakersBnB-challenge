14665a81373833596a72c394064dec529654abc7
Stefan Liute <stefanliute@gmail.com>
2017-02-09 18:15:43 +0000

    Approve/Reject completed

--- lib/controllers/users.rb
+++ lib/controllers/users.rb
@@ -18,16 +18,20 @@ class MakersBnB < Sinatra::Base
   post '/users/my_account' do
     # @spaces = current_user ? current_user.spaces(order: :created_at.desc) : []
     @listed_spaces = current_user ? current_user.spaces(order: :created_at.desc) : []
-    @rented_spaces = Space.all(rented_by: current_user.id)
+    @reqs = Request.all(user_id: current_user.id)
+    @pending_reqs = @reqs.all(status: 'Pending')
+    @accepted_reqs = @reqs.all(status: 'Approved')
+    # @rented_spaces = Space.all(rented_by: current_user.id)
     erb :'users/my_account'
   end
 
   get '/users/my_account' do
     # @spaces = current_user ? current_user.spaces(order: :created_at.desc) : []
     @listed_spaces = current_user ? current_user.spaces(order: :created_at.desc) : []
-    @rented_spaces = Space.all(rented_by: current_user.id)
     @reqs = Request.all(user_id: current_user.id)
     @pending_reqs = @reqs.all(status: 'Pending')
+    @accepted_reqs = @reqs.all(status: 'Approved')
+    # @rented_spaces = Space.all(rented_by: current_user.id)
     erb :'users/my_account'
   end
 end
--- spec/features/approve_spec.rb
+++ spec/features/approve_spec.rb
@@ -24,4 +24,45 @@ feature 'Approving requests' do
     expect(page).to have_button 'Approve'
     expect(page).to have_button 'Reject'
   end
+
+  scenario 'User can approve a specific request' do
+    sign_in_list_sign_out(email: user.email, password: user.password)
+    sign_in(email: user2.email, password: user2.password)
+    visit '/'
+    # use datepicker to pick a date here... such as 2017-11-10 (Nov 10, one day before end_date)
+    # datepicker should use min and max based on this space's start and end dates.
+    fill_in :request_date, with: '2017-11-10'
+    click_button 'Rent'
+    click_button 'Sign Out'
+    sign_in(email: user.email, password: user.password)
+    visit '/users/my_account'
+    click_button 'View Requests'
+    click_button 'Approve'
+    click_button 'Sign Out'
+    sign_in(email: user2.email, password: user2.password)
+    visit '/users/my_account'
+    expect(page).to have_content 'You have booked:'
+  end
+
+  scenario 'User can reject a specific request' do
+    sign_in_list_sign_out(email: user.email, password: user.password)
+    sign_in(email: user2.email, password: user2.password)
+    visit '/'
+    # use datepicker to pick a date here... such as 2017-11-10 (Nov 10, one day before end_date)
+    # datepicker should use min and max based on this space's start and end dates.
+    fill_in :request_date, with: '2017-11-10'
+    click_button 'Rent'
+    click_button 'Sign Out'
+    sign_in(email: user.email, password: user.password)
+    visit '/users/my_account'
+    click_button 'View Requests'
+    click_button 'Reject'
+    click_button 'Sign Out'
+    sign_in(email: user2.email, password: user2.password)
+    visit '/users/my_account'
+    expect(page).not_to have_content 'You have booked:'
+    expect(page).not_to have_content 'Nice condo!'
+    expect(page).not_to have_content 'A lovely one-bedroom condo in southern France, far from foggy London.'
+    expect(page).not_to have_content '5'
+  end
 end
